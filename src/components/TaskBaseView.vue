92a672753b6076b374e790d8be54e57689d4dbb0
张晨辉 <10119733+ach3084@user.noreply.gitee.com>
2025-11-17 11:23:54 +0800

    添加 Element UI 库并重构应用程序布局 - 通过在 package.json 和 package-lock.json 中添加依赖项，将 Element UI 集成到项目中。 - 更新 main.js 以包含 Element UI 及其样式。 - 创建了一个新的导航组件，以改进布局和路由。 - 添加了多个新视图，用于管理不同的数据类别（知识库、规则库、任务库等）。 - 移除了未使用的 HomeView 和 AboutView 组件，以简化应用程序。 - 简化了 App.vue 模板，移除了导航链接，转而使用专门的导航组件。

---
+++ src/components/TaskBaseView.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <h1>任务库</h1>
+</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,64 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Navigation from '../views/Navigation.vue'
+import RuleBaseView from '../components/RuleBaseView.vue'
+import KnowledgeBaseView from '../components/KnowledgeBaseView.vue'
+import TaskBaseView from '../components/TaskBaseView.vue'
+import WarningBaseView from '../components/WarningBaseView.vue'
+import DataBaseView from '../components/DataBaseView.vue'
+import LogBaseView from '../components/LogBaseView.vue'
+import PermissionBaseView from '../components/PermissionBaseView.vue'
+import ModelBaseView from '../components/ModelBaseView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: Navigation,
+    redirect: '/home',
+    children: [
+      {
+        path: 'knowledge-base',
+        name: 'knowledge-base',
+        component: KnowledgeBaseView
+      },
+      {
+        path: 'rule-base',
+        name: 'rule-base',
+        component: RuleBaseView
+      },
+      {
+        path: 'model-base',
+        name: 'model-base',
+        component: ModelBaseView
+      },
+      {
+        path: 'task-base',
+        name: 'task-base',
+        component: TaskBaseView
+      },
+      {
+        path: 'warning-base',
+        name: 'warning-base',
+        component: WarningBaseView
+      },
+      {
+        path: 'data-base',
+        name: 'data-base',
+        component: DataBaseView
+      },
+      {
+        path: 'log-base',
+        name: 'log-base',
+        component: LogBaseView
+      },
+      {
+        path: 'permission-base',
+        name: 'permission-base',
+        component: PermissionBaseView
+      }
+    ]
   }
 ]
 
